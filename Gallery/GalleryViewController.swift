891e8475537a473109b78d11ee8efc5da4cbfa41
Oleg Ovechkin <oovechkin@gmail.com>
2017-04-21 19:46:19 +0300

    rename to GalleryViewController

---
+++ Gallery/GalleryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Олег Овечкин on 21/04/2017.
+//  Copyright © 2017 Spent. All rights reserved.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //
+    }
+
+}
+
